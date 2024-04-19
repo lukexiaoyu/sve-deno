ac71420f5ee5b2ff2f1f34440f192d0258214241
lukexiao <814008000@163.com>
2024-04-19 11:28:12 +0800

    1

--- src/routes/sb/+page.server.js
+++ src/routes/sb/+page.server.js
@@ -1,5 +1,5 @@
 /** @type {import('./$types').PageServerLoad} */
-import {sb} from 'npm:kl_sb'
+import {sb} from 'kl_sb'
 export async function load() {
     const {data,error}=await sb
         .from('peos')
